f0853e96061052eebbe9a69060f72b466241c116
Joel Matias <joelsantiagos000@gmail.com>
2026-03-17 19:37:08 -0600

    ui: Change 'Hablemos' to 'Trabajemos' in Contact section

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -80,7 +80,7 @@ export function Contact() {
               color: "var(--text)",
             }}
           >
-            Hablemos
+            Trabajemos
             <br />
             <span style={{ color: "var(--lime)" }}>juntos</span>
           </h2>
